de68a20cd8eaa45b7138f159b64106043c57626c
Javier Sánchez <javistrabajosjlg@gmail.com>
2026-08-24 22:02:38 +0200

    Starting PlayerManager Script

--- ProjectMovement/Assets/_MyAssets/Scripts/PlayerManager.cs
+++ ProjectMovement/Assets/_MyAssets/Scripts/PlayerManager.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    InputSystem_Actions inputActions;
+
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+        inputActions = GetComponent<InputSystem_Actions>();
+
+
+
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
     }
 }
